34add39b28fd9f8764bb12c641d6c9ba27dd46fc
eugenpetrov <petroveugenv@gmail.com>
2017-05-31 14:45:40 +0300

    Snippet_Controller small updates, composer file was added

--- src/Snippet/Controller/Controller/Adminhtml/Custom/Index.php
+++ src/Snippet/Controller/Controller/Adminhtml/Custom/Index.php
@@ -4,6 +4,10 @@ namespace Snippet\Controller\Controller\Adminhtml\Custom;
 
 use Magento\Backend\App\Action;
 
+/**
+ * Class Index
+ * @package Snippet\Controller\Controller\Adminhtml\Custom
+ */
 class Index extends Action
 {
     public function execute()
